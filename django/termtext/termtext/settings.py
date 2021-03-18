822b54b81a66b0cafd4f65f9e7fe21ddc04ba357
Taichen <jatr812@gmail.com>
2021-03-18 09:59:55 -0700

    Pushing project

--- django/termtext/termtext/settings.py
+++ django/termtext/termtext/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v)b1xl84_mg%o+t!)d@$#x@&!-!w31f2x%lt3e&0vzz_02p^2!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'termtext_frontend',
 ]
 
 MIDDLEWARE = [
